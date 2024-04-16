4b49b87432b441829877e2b05a74c29c2a209693
Aleksander Tarczynski <olek.kt@gmail.com>
2024-04-16 16:17:15 -0400

    establish basic foot directions

--- foot_pressure/foot_pressure.ino
+++ foot_pressure/foot_pressure.ino
@@ -1,11 +1,33 @@
 int s1 = A0;
-int s1_val = 0;
+int left = 0;
 
 int s2 = A1;
-int s2_val = 0;
+int heel = 0;
 
 int s3 = A2;
-int s3_val = 0;
+int right = 0;
+
+int check(int a, int b, int c) {
+  // float rat1 = abs((a/b) - 1);
+  // float rat2 = abs((b/c) - 1);
+  // float rat3 = abs((c/a) - 1);
+  float rat1 = abs(a-b);
+  float rat2 = abs(b-c);
+  float rat3 = abs(c-a);
+  return (rat1 + rat2 + rat3) < 50;
+}
+
+char* direction(int a, int b, int c) {
+  if (a > 700 && b < 20 && c > 700)
+    return "forward";
+  if (a < 20 && b > 200 && c < 100)
+    return "backward";
+  if (a < 50 && b < 200 && c > 700)
+    return "right";
+  if (a > 700 && b < 200 && c < 500)
+    return "left";
+  return "center";
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,14 +36,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  s1_val = analogRead(s1);
-  s2_val = analogRead(s2);
-  s3_val = analogRead(s3);
+  left = analogRead(s1);
+  heel = analogRead(s2);
+  right = analogRead(s3);
 
   delay(100);
-  Serial.print(s1_val);
+  Serial.print(direction(left, heel, right));
   Serial.print(",");
-  Serial.print(s2_val);
+  Serial.print(left);
   Serial.print(",");
-  Serial.println(s3_val);
+  Serial.print(heel);
+  Serial.print(",");
+  Serial.println(right);
 }
